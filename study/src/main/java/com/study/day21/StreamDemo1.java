da78f9fe996214a62d1f3cd4057af123b01910e7
user <wu12541223@gmail.com>
2021-08-05 10:02:32 +0800

    Java

---
+++ study/src/main/java/com/study/day21/StreamDemo1.java
@@ -0,0 +1,46 @@
+package com.study.day21;
+
+import java.util.Arrays;
+import java.util.IntSummaryStatistics;
+
+public class StreamDemo1 {
+
+	public static void main(String[] args) {
+		String[] names = {"Jo", "John", "Jackson", "Vincent", "Anita", "Alex"};
+		
+		Arrays.stream(names).forEach(name -> System.out.println(name));
+		
+		System.out.println("1.-------------");
+		
+		Arrays.stream(names)
+			.filter(name -> name.length() > 4)
+			.forEach(name -> System.out.println(name));
+		
+		System.out.println("2.-------------");
+		// 印出每一個名字的長度 case 1
+		Arrays.stream(names).forEach(name -> System.out.println(name.length())); //ObjectStream
+		
+		System.out.println("3.-------------");
+		// 印出每一個名字的長度 case 2
+		Arrays.stream(names)
+			.mapToInt(name -> name.length())    //IntStream
+			.forEach(name -> System.out.println(name));
+		
+		System.out.println("4.-------------");
+		// 求出每一個名字的長度的總和
+		int sum = Arrays.stream(names)
+			.mapToInt(name -> name.length())
+			.sum();
+		System.out.println(sum);
+		System.out.println("5.-------------");
+		// 統計資料
+		IntSummaryStatistics stat = Arrays.stream(names)
+			.mapToInt(name -> name.length())
+			.summaryStatistics();
+		System.out.println(stat);
+		System.out.println(stat.getAverage());
+		System.out.println(stat.getSum());
+		System.out.println(stat.getMax());
+		System.out.println(stat.getMin());
+	}
+}
---
+++ study/src/main/java/com/study/day22/MethodClass.java
@@ -0,0 +1,35 @@
+package com.study.day22;
+
+class Job{
+	
+	//方法
+	void start() {
+		
+		//方法內部類別
+		class Report{
+			
+			void content() {
+				System.out.println("年度報告");
+			}
+		}
+		//建立
+		Report report = new Report();
+		report.content();
+	}
+	
+	void other(){
+		class Report{
+			
+		}
+	}
+}
+
+public class MethodClass {
+
+	public static void main(String[] args) {
+		Job job = new Job();
+		job.start();
+
+	}
+
+}
